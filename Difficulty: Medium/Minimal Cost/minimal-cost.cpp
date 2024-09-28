5fea88bc77a2ea58dce186b905d980dc15bdea15
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-09-28 11:11:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimal Cost/minimal-cost.cpp
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -6,24 +6,21 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-   int f(int i,int j,vector<int>& h,vector<int>&dp){
-    //   if(i>=n) return 0;
-    int n=h.size();
-       if(i>=n-1) return 0;
-       
-       if(dp[i]!=-1) return dp[i];
-       int ans=INT_MAX;
-       for(int k=1;k<=j;k++){
-           if(i+k<n){
-               ans=min(ans,abs(h[i]-h[i+k])+f(i+k,j,h,dp));
-           }
+   int f(int i,int k, vector<int>& arr,vector<int>&dp){
+        
+        if(i>=arr.size()) return 1e9;
+         if(i==arr.size()-1) return 0;
+         if(dp[i]!=-1) return dp[i];
+       int ans=1e9;
+       for( int j=1;j<=k;j++){
+           int t=abs(arr[i]-arr[i+j])+f(i+j,k,arr,dp);
+           ans=min(ans,t);
        }
        return dp[i]=ans;
    }
-    int minimizeCost(vector<int>& arr, int& k) {
+    int minimizeCost(int k, vector<int>& arr) {
         // Code here
-        int n=arr.size();
-        vector<int>dp(n,-1);
+        vector<int>dp(arr.size(),-1);
         return f(0,k,arr,dp);
     }
 };
@@ -47,7 +44,7 @@ int main() {
             arr.push_back(number);
         }
         Solution obj;
-        int res = obj.minimizeCost(arr, k);
+        int res = obj.minimizeCost(k, arr);
         cout << res << endl;
         // string tl;
         // getline(cin, tl);
