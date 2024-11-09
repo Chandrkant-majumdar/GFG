e09abcc53db9d03848544901f97580856ce41592
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-09 12:09:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -6,28 +6,39 @@ using namespace std;
 class Solution{
 
 	public:
+	int mod=1e9+7;
 	int f(int i,int sum,int arr[],vector<vector<int>>&dp){
-	   // if(i==0 && sum==0) return 2;
-	   //if(sum==0) return 1;
-	   int mod=1e9+7;
-	    if(i==0){
-	        if(sum==0 && arr[0]==0 ) return 2;
-	        if(sum==0 || sum==arr[0]) return 1;
-	        return 0;
-	    } 
-	    if(dp[i][sum]!=-1) return dp[i][sum];
-	    int tk=(sum>=arr[i])?f(i-1,sum-arr[i],arr,dp):0;
-	    int nt=f(i-1,sum,arr,dp);
-	    return dp[i][sum]=(tk+nt)%mod;
-	}
+        // if(sum==arr[i]) return 1;
+        // if(i==0){
+        if(i==0){
+            if(sum==0 && arr[i]==0) return 2;
+            if(sum==0 || sum==arr[i]) return 1;
+            return 0;
+        }
+       
+           
+        //   if(sum==0 && arr[i]==sum) return 2;
+           
+        //   return arr[i]==sum;
+            
+        
+        
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        
+        int tk=(sum>=arr[i])?f(i-1,sum-arr[i],arr,dp):0;
+        int nt=f(i-1,sum,arr,dp);
+        
+        return dp[i][sum]=(tk+nt)%(mod);
+    }
 	int perfectSum(int arr[], int n, int sum)
 	{
-	    vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        // Your code goes here
+        //  int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
         return f(n-1,sum,arr,dp);
 	}
 	  
 };
-
 
 //{ Driver Code Starts.
 int main() 
@@ -51,7 +62,9 @@ int main()
 	    Solution ob;
 	    cout << ob.perfectSum(a, n, sum) << "\n";
 	     
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 
