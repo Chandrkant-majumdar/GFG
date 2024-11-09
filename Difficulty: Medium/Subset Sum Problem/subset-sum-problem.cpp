a37b419d8ebe8e65f8a1a253e4bf8367170057ce
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-09 11:24:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,52 +1,65 @@
 //{ Driver Code Starts
-//Initial template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
+
 
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
-class Solution{   
-public:
-    bool f(int i,int sum,vector<int>&arr, vector<vector<int>>&dp){
-        if(sum==0) return true;
+class Solution {
+  public:
+    bool f(int i,int sum,vector<int>& arr,vector<vector<int>>&dp){
+        if(sum==0) return 1;
         if(i==0){
+            return (sum==arr[i]);
             
-            return sum==arr[0];
         }
+        
         if(dp[i][sum]!=-1) return dp[i][sum];
-        bool tk=(sum>=arr[i])?f(i-1,sum-arr[i],arr,dp):false;
+        
+        bool tk=(sum>=arr[i])?f(i-1,sum-arr[i],arr,dp):0;
         bool nt=f(i-1,sum,arr,dp);
-        return dp[i][sum]=(tk||nt);
+        
+        return dp[i][sum]=tk|| nt;
     }
-    bool isSubsetSum(vector<int>arr, int sum){
-        // code here 
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
         int n=arr.size();
         vector<vector<int>>dp(n,vector<int>(sum+1,-1));
         return f(n-1,sum,arr,dp);
     }
 };
 
+
 //{ Driver Code Starts.
-int main() 
-{ 
+
+int main() {
+
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int N, sum;
-        cin >> N;
-        vector<int> arr(N);
-        for(int i = 0; i < N; i++){
-            cin >> arr[i];
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
+        int sum;
         cin >> sum;
-        
+        cin.ignore();
+
         Solution ob;
-        cout << ob.isSubsetSum(arr, sum) << endl;
+        if (ob.isSubsetSum(arr, sum))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
     }
-    return 0; 
-} 
+    return 0;
+}
 
 // } Driver Code Ends
