b4f191b3390068bba9c205a48b1dd6708c301581
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-11 23:08:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -2,67 +2,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    // Method 1 (using BFS)
-    bool bfs(int V,vector<int>adj[],vector<int>&vis){
-         // Code here
-        // vector<int>vis(V,0);
+    // bool helper()
+    bool isCycle(vector<vector<int>>& adj) {
+        // Code here
+        int n=adj.size();
+        vector<int>vis(n,0);
         queue<pair<int,int>>q;
-        q.push({V,-1});
-        vis[V]=1;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                 q.push({i,-1});
+        vis[i]=1;
         while(!q.empty()){
-            int node=q.front().first;
-            int par=q.front().second;
+            auto t=q.front();
             q.pop();
-            for(auto nbr:adj[node]){
-                if(!vis[nbr]){
-                    vis[nbr]=1;
-                    q.push({nbr,node});
-                }else if(nbr!=par){
+            int node=t.first;
+            int par=t.second;
+            for(auto x:adj[node]){
+                if(!vis[x]){
+                    vis[x]=1;
+                    q.push({x,node});
+                }
+                else if(x!=par){
                     return true;
                 }
             }
         }
-        return false;
-    }
-    bool isCycle1(int V, vector<int> adj[]) {
-       vector<int>vis(V,0);
-       for(int i=0;i<V;i++){
-           if(!vis[i]){
-               if(bfs(i,adj,vis))
-               return true;
-           }
-       }
-       return false;
-    }
-    
-    // Method 2
-    bool dfs(int node,int par,vector<int>&vis,vector<int>adj[]){
-        vis[node]=1;
-        for(auto nbr:adj[node]){
-            if(!vis[nbr]){
-                if(dfs(nbr,node,vis,adj)) return true;
-            }else if(nbr!=par){
-                return true;
             }
         }
-        
+       
         return false;
     }
-    bool isCycle(int V, vector<int> adj[]){
-        vector<int>vis(V,0);
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,-1,vis,adj)) return true;
-            }
-            
-        }
-        return false;
-    }
-    
 };
 
 //{ Driver Code Starts.
@@ -72,7 +46,7 @@ int main() {
     while (tc--) {
         int V, E;
         cin >> V >> E;
-        vector<int> adj[V];
+        vector<vector<int>> adj(V);
         for (int i = 0; i < E; i++) {
             int u, v;
             cin >> u >> v;
@@ -80,11 +54,14 @@ int main() {
             adj[v].push_back(u);
         }
         Solution obj;
-        bool ans = obj.isCycle(V, adj);
+        bool ans = obj.isCycle(adj);
         if (ans)
             cout << "1\n";
         else
             cout << "0\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
