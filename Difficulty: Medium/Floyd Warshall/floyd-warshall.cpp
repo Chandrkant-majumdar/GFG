0e43931be707c1cb223fd3b808e68caa7957bb53
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-17 11:50:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -1,68 +1,68 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
 class Solution {
   public:
-	void shortest_distance(vector<vector<int>>&m){
-	    // Code here
-	    int n=m.size();
-	   // int m=ma[0].size();
-	   for(int i=0;i<n;i++){
-	       for(int j=0;j<n;j++){
-	           if(m[i][j]==-1){
-	               m[i][j]=1e8;
-	           }
-	       }
-	   }
-	   
-	   for(int k=0;k<n;k++){
-	       for(int i=0;i<n;i++){
-	           for(int j=0;j<n;j++){
-	               
-	               m[i][j]=min(m[i][k]+m[k][j],m[i][j]);
-	               
-	           }
-	       }
-	   }
-	   for(int i=0;i<n;i++){
-	       for(int j=0;j<n;j++){
-	           if(m[i][j]==1e8){
-	               m[i][j]=-1;
-	           }
-	       }
-	   }
-	   
-	}
+    void shortestDistance(vector<vector<int>>& mat) {
+        // Code here
+        int n=mat.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==-1){
+                    mat[i][j]=1e9;
+                }
+            }
+        }
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1e9){
+                    mat[i][j]=-1;
+                }
+            }
+        }
+        
+    }
 };
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>matrix(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cin >> matrix[i][j];
-			}
-		}
-		Solution obj;
-		obj.shortest_distance(matrix);
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cout << matrix[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        obj.shortestDistance(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
